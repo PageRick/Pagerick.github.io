511cd78f299292f233d02b4d4929796fe4f977ea
PageRick <boyricky2007@gmail.com>
2024-07-03 10:24:08 -0700

    Clearing up old stuffs

--- script/script.js
+++
@@ -1,7 +0,0 @@
-function ownerMessage() {
-  alert(`This is the owner's Phone number, Use this to activate and show the Whatsapp bot Commands (.help or .menu)`)
-}
-
-function message() {
-  alert('You are about to be redirected to another site, Please remember to be safe in foreign sites, Thank you!');
-}
